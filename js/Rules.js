d2216ba1d71907741467659da27936298aa2387c
Damian Kierzkowski <damiankierzkowski08@gmail.com>
2019-08-28 23:55:44 +0200

    Create Rules.js and create basic sceletons of two methods for rules

---
+++ js/Rules.js
@@ -0,0 +1,9 @@
+class Rules {
+    static upperPartOfStatsTable(countedDices, firstColumn, i) {
+        console.log("górna");
+    }
+
+    static lowerPartOfStatsTable(countedDices, firstColumn, i) {
+        console.log("dolna");
+    }
+}
