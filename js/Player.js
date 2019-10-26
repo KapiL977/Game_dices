cff1be208125c116d3c559df03354f97fa60dbc3
Damian Kierzkowski <damiankierzkowski08@gmail.com>
2019-10-27 00:12:27 +0200

    fix: user can start game without giving players names

--- js/Player.js
+++ js/Player.js
@@ -11,7 +11,7 @@ class Player {
         const playersNames = [];
         for (let i = 0; i < numberOfPlayers; i++) {
             let playerName = prompt(`Podaj imię zawodnika ${i + 1} (ograniczenie znaków: max 8, min 1)`);
-            while (playerName.length < 1 || playerName.length > 8) {
+            while (playerName === null || (playerName.length < 1 || playerName.length > 8)) {
                 playerName = prompt(`Podano za krótkie lub za długie imię! Podaj imię zawodnika ${i + 1} (ograniczenie znaków: max 8, min 1)`);
             }
             playersNames.push(playerName);
