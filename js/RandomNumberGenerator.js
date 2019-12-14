6b03aebc6dac2c4d36ec4748259cc2820b3ab6dc
KapiL977 <58886715+KapiL977@users.noreply.github.com>
2019-12-14 17:44:14 +0100

    Update RandomNumberGenerator.js

--- js/RandomNumberGenerator.js
+++ js/RandomNumberGenerator.js
@@ -2,7 +2,7 @@ export default class RandomNumberGenerator {
     generateRandomNumbers(number) {
         const randomNumbers = [];
         for (let i = 0; i < number; i++) {
-            randomNumbers.push(Math.floor(Math.random() * 6) + 1);
+            randomNumbers.push(Math.floor(Math.random() * 7) + 1);
         }
         return randomNumbers;
     }
